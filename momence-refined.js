fbe70501929aadc90562a6b9aa4b4ee5e6ce02de
Kwamzilla <41638604+Kwamzilla@users.noreply.github.com>
2023-01-03 02:35:11 +0800

    Update momence-refined.js

--- momence-refined.js
+++ momence-refined.js
@@ -1,69 +1,102 @@
 $(document).ready(function() {
-  const hostId = '21671';
-  const API_URL = `https://app.momence.com/api/v1/hosts/${hostId}/schedule?expand=session`;
+let hostId = $("script[host_id]").attr("host_id");
+let teacherIds = $("script[teacher_ids]").attr("teacher_ids");
+let locationIds = $("script[location_ids]").attr("location_ids");
+let tagIds = $("script[tag_ids]").attr("tag_ids");
+let defaultFilter = $("script[default_filter]").attr("default_filter");
+let src = $("script[src]").attr("src");
 
-  function getSchedule() {
-    $.get(API_URL, function(data) {
-      const schedule = data.items;
+$("#ribbon-schedule").append(`
+  <momence-host-schedule 
+    host-id="${hostId}" 
+    teacher-ids="${teacherIds}" 
+    location-ids="${locationIds}" 
+    tag-ids="${tagIds}" 
+    default-filter="${defaultFilter}" 
+    src="${src}"
+  ></momence-host-schedule>
+`);
 
-      schedule.forEach(function(item) {
-        const startDate = new Date(item.start_date);
-        const endDate = new Date(item.end_date);
-        const startTime = startDate.toLocaleTimeString('en-US', {
-          hour: 'numeric',
-          minute: 'numeric',
-          hour12: true
-        });
-        const endTime = endDate.toLocaleTimeString('en-US', {
-          hour: 'numeric',
-          minute: 'numeric',
-          hour12: true
-        });
-        const day = startDate.toLocaleDateString('en-US', { weekday: 'long' });
-        const month = startDate.toLocaleDateString('en-US', { month: 'long' });
-        const date = startDate.toLocaleDateString('en-US', { day: 'numeric' });
-        const sessionId = item.id;
-        const sessionType = item.session.type;
-        const sessionName = item.session.name;
-        const sessionDescription = item.session.description;
-        const sessionImage = item.session.image_url;
-        const sessionTeacher = item.session.teacher.name;
-        const sessionCapacity = item.session.capacity;
-        const sessionAttendees = item.session.attendee_count;
-        const sessionWaitlist = item.session.waitlist_count;
-        const sessionStatus = item.session.status;
-        const sessionLink = item.session.url;
+const hostSchedule = document.querySelector("momence-host-schedule");
 
-        if (sessionType === 'class') {
-$('#momence-plugin-host-schedule').append(
-'<article class="momence-host_schedule-session_list-item" data-session_id="' +
-           sessionId +
-           '"><div class="momence-host_schedule-session_list-image_container"><img src="' +
-           sessionImage +
-           '" class="momence-host_schedule-session_list-item-image"></div><div class="momence-host_schedule-session_list-second-section"><div class="momence-host_schedule-session_list-section-header"><img src="' +
-           sessionImage +
-           '" class="momence-host_schedule-session_list-item-image"><div><h3 class="momence-host_schedule-session_list-item-type">Class</h3><h4 class="momence-host_schedule-session_list-item-title">' +
-sessionName +
-'</h4></div></div><div class="momence-host_schedule-session_list-item-info momence-session-teacher"><svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="momence-host_schedule-session_list-item-info-icon"><g clip-path="url(#icon-profile-filled_svg__clip0_220_27685)"><rect x="1" y="1" width="18" height="18" rx="9" fill="currentColor"></rect><circle cx="10" cy="7.75" r="3.15" fill="#fff"></circle><path d="M18.25 18.25a8.25 8.25 0 01-16.5 0c0-4.556 3.694-6 8.25-6s8.25 1.444 8.25 6z" fill="#fff"></path></g><rect x="1.9" y="1.9" width="16.2" height="16.2" rx="8.1" stroke="currentColor" stroke-width="1.8"></rect><defs><clipPath id="icon-profile-filled_svg__clip0'" class="momence-host_schedule-session_list-item-image"><div><h3 class="momence-host_schedule-session_list-item-type">Class</h3><h4 class="momence-host_schedule-session_list-item-title">' +
-              sessionName +
-              '</h4></div></div><div class="momence-host_schedule-session_list-item-info momence-session-teacher"><svg class="momence-host_schedule-session_list-item-info-icon"><use xlink:href="#icon-profile-filled"></use></svg><div class="momence-host_schedule-session_list-item-info-text">' +
-              sessionTeacher +
-              '</div></div><div class="momence-host_schedule-session_list-item-info momence-session-location"><svg class="momence-host_schedule-session_list-item-info-icon"><use xlink:href="#icon-location-filled"></use></svg><div class="momence-host_schedule-session_list-item-info-text">' +
-              sessionLocation +
-              '</div></div><div class="momence-host_schedule-session_list-item-info momence-session-time"><svg class="momence-host_schedule-session_list-item-info-icon"><use xlink:href="#icon-clock-filled"></use></svg><div class="momence-host_schedule-session_list-item-info-text">' +
-              sessionDate +
-              '</div></div><div class="momence-host_schedule-session_list-item-book-cta"><a href="' +
-              sessionBookLink +
-              '" target="_blank" class="momence-host_schedule-session_list-item-book-button">Book</a></div></article>'
-          );
-          } else if (sessionType === 'event') {
-          $('#momence-plugin-host-schedule').append(
-            '<article class="momence-host_schedule-session_list-item" data-session_id="' +
-              sessionId +
-              '"><div class="momence-host_schedule-session_list-image_container"><img src="' +
-              sessionImage +
-              '" class="momence-host_schedule-session_list-item-image"></div><div class="momence-host_schedule-session_list-second-section"><div class="momence-host_schedule-session_list-section-header"><img src="' +
-              sessionImage +
-              '" class="momence-host_schedule-session_list-item-image"><div><h3 class="momence-host_schedule-session_list-item-type">Event</h3><h4 class="momence-host_schedule-session_list-item-title">' +
-              sessionTitle +
-              '</h4></div></div>
+hostSchedule.addEventListener("loaded", () => {
+  const schedule = hostSchedule.shadowRoot.querySelector("#schedule");
+  const filters = hostSchedule.shadowRoot.querySelector("#filters");
+  const filterButtons = hostSchedule.shadowRoot.querySelectorAll(
+    ".btn-filter"
+  );
+  const events = hostSchedule.shadowRoot.querySelectorAll(".event");
+  const views = hostSchedule.shadowRoot.querySelectorAll(".view");
+  const viewButtons = hostSchedule.shadowRoot.querySelectorAll(
+    ".btn-view"
+  );
+  let currentView = "week";
+  let currentFilter = defaultFilter;
+
+  // apply active class to current view button
+  viewButtons.forEach(button => {
+    if (button.getAttribute("data-view") === currentView) {
+      button.classList.add("active");
+    }
+  });
+
+  // apply active class to current filter button
+  filterButtons.forEach(button => {
+    if (button.getAttribute("data-filter") === currentFilter) {
+      button.classList.add("active");
+    }
+  });
+
+  // handle click on filter button
+  filterButtons.forEach(button => {
+    button.addEventListener("click", e => {
+      // remove active class from all filter buttons
+      filterButtons.forEach(b => b.classList.remove("active"));
+      // add active class to clicked button
+      e.target.classList.add("active");
+      // update current filter
+      currentFilter = e.target.getAttribute("data-filter");
+      // apply filter
+      applyFilter();
+    });
+  });
+
+  // handle click on view button
+  viewButtons.forEach(button => {
+    button.addEventListener("click", e => {
+      // remove active class from all view buttons
+      viewButtons.forEach(b => b.classList.remove("active"));
+
+              // add active class to clicked button
+      e.target.classList.add("active");
+      // update current view
+      currentView = e.target.getAttribute("data-view");
+      // apply view
+      applyView();
+    });
+  });
+
+  const applyFilter = () => {
+    events.forEach(event => {
+      event.style.display = "none";
+      if (currentFilter === "show-all") {
+        event.style.display = "block";
+      } else if (event.classList.contains(currentFilter)) {
+        event.style.display = "block";
+      }
+    });
+  };
+
+  const applyView = () => {
+    views.forEach(view => {
+      view.style.display = "none";
+      if (view.classList.contains(currentView)) {
+        view.style.display = "block";
+      }
+    });
+  };
+
+  applyFilter();
+  applyView();
+});
+});
